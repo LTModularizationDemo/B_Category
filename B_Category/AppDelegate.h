28111d991df10aff88eb46dd0f925f61276e90a5
Nanmenhaishao <708849329@qq.com>
2018-01-25 19:56:33 +0800

    Initial Commit

---
+++ B_Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  B_Category
+//
+//  Created by 张常伟 on 2018/1/25.
+//  Copyright © 2018年 张常伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ B_Category/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  B_Category
+//
+//  Created by 张常伟 on 2018/1/25.
+//  Copyright © 2018年 张常伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
